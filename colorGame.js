427f2954fd7943144c75fd63b06d9e169c9e9c38
csm2017 <mcgahancory@gmail.com>
2020-07-30 18:14:42 -0500

    added difficulty buttons and some styling

--- colorGame.js
+++ colorGame.js
@@ -3,6 +3,10 @@ const squareContainer = document.getElementById('container');
 const rgbDisplay = document.getElementById('rgbDisplay');
 const messageDisplay = document.getElementById('message');
 const h1 = document.querySelector('h1');
+const resetButton = document.getElementById('reset');
+const easyBtn = document.getElementById('easyBtn');
+const hardBtn = document.getElementById('hardBtn');
+const stripe = document.querySelector('#stripe');
 
 /***
  * FUNCTIONS
@@ -44,7 +48,8 @@ const generateRandomColors = num => {
 /**
  * INITIALIZE
  */
-let colors = generateRandomColors(6);
+let numOfSquares = 6;
+let colors = generateRandomColors(numOfSquares);
 let pickedColor = pickColor();
 
 rgbDisplay.textContent = pickedColor;
@@ -54,7 +59,7 @@ for(let i = 0; i < squares.length; i++) {
 }
 
 /**
- * LISTENER
+ * LISTENERS
  */
 squareContainer.addEventListener('click', e => {
     const square = e.target.closest('.square');
@@ -63,6 +68,7 @@ squareContainer.addEventListener('click', e => {
     //Correct Answer!
     if(clickedColor === pickedColor){
         messageDisplay.textContent = 'Correct!';
+        resetButton.textContent = 'Play Again?';
         changeColors(clickedColor);
     } 
     //Wrong Answer
@@ -71,4 +77,54 @@ squareContainer.addEventListener('click', e => {
         messageDisplay.textContent = 'Try Again';
     }
 
+});
+
+resetButton.addEventListener('click', () => {
+    //generate all new colors
+    colors = generateRandomColors(numOfSquares);
+    //pick a new random color
+    pickedColor = pickColor();
+    //change heading color text
+    rgbDisplay.textContent = pickedColor;
+    //change colors of squares
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+
+    h1.style.backgroundColor = '#232323';
+});
+
+stripe.addEventListener('click', e => {
+    const button = e.target.closest('button');
+    if (button === easyBtn) {
+        easyBtn.classList.add('selected');
+        hardBtn.classList.remove('selected');
+        numOfSquares = 3;
+        colors = generateRandomColors(numOfSquares);
+        pickedColor = pickColor();
+
+        rgbDisplay.textContent = pickedColor;
+        for(let i = 0; i  < squares.length; i++) {
+            if(colors[i]) {
+                squares[i].style.backgroundColor = colors[i];
+            }
+            else {
+                squares[i].style.display = 'none';
+            }
+        }
+    }
+    else if (button === hardBtn) {
+        easyBtn.classList.remove('selected');
+        hardBtn.classList.add('selected');
+        numOfSquares = 6;
+        colors = generateRandomColors(numOfSquares);
+        pickedColor = pickColor();
+
+        rgbDisplay.textContent = pickedColor;
+        for(let i = 0; i  < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = 'block';
+        }
+    }
+    
 });
