b327fa26462b4216fce544188e3349bcbf84c033
csm2017 <mcgahancory@gmail.com>
2020-07-30 17:01:43 -0500

    added randomization to color choices

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,8 @@
-let colors = ['rgb(255, 0, 0)'
-            ,'rgb(255, 255, 0)'
-            ,'rgb(0, 255, 0)'
-            ,'rgb(0, 255, 255)'
-            ,'rgb(0, 0, 255)'
-            ,'rgb(255, 0, 255)'];
-
 const squares = document.querySelectorAll('.square');
 const squareContainer = document.getElementById('container');
 const rgbDisplay = document.getElementById('rgbDisplay');
 const messageDisplay = document.getElementById('message');
+const h1 = document.querySelector('h1');
 
 /***
  * FUNCTIONS
@@ -18,16 +12,39 @@ const changeColors = color => {
     for(let i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = color;
     }
+    h1.style.backgroundColor = color;
 };
 
 const pickColor = () => {
     const num = Math.floor(Math.random() * colors.length);
     return colors[num];
 };
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+const generateRandomColors = num => {
+    //make an array
+    let arr = [];
+    //add num random colors to array
+    for (let i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr;
+};
+
+
 
 /**
  * INITIALIZE
  */
+let colors = generateRandomColors(6);
 let pickedColor = pickColor();
 
 rgbDisplay.textContent = pickedColor;
@@ -43,10 +60,13 @@ squareContainer.addEventListener('click', e => {
     const square = e.target.closest('.square');
     const clickedColor = square.style.backgroundColor;
 
+    //Correct Answer!
     if(clickedColor === pickedColor){
         messageDisplay.textContent = 'Correct!';
         changeColors(clickedColor);
-    } else {
+    } 
+    //Wrong Answer
+    else {
         square.style.backgroundColor = '#232323';
         messageDisplay.textContent = 'Try Again';
     }
