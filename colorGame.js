0a6d3062855a7a7948eeb1570f4312530a7595a6
csm2017 <mcgahancory@gmail.com>
2020-07-30 19:41:10 -0500

    styling changes

--- colorGame.js
+++ colorGame.js
@@ -91,7 +91,7 @@ resetButton.addEventListener('click', () => {
         squares[i].style.backgroundColor = colors[i];
     }
 
-    h1.style.backgroundColor = '#232323';
+    h1.style.backgroundColor = 'steelblue';
 });
 
 stripe.addEventListener('click', e => {
